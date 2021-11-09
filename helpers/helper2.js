974579cc00b2b44c9846b6e79e418d3dfd107741
Shinoda <korotkoshey@gmail.com>
2021-11-09 23:57:48 +0200

    First commit

--- helpers/helper2.js
+++ helpers/helper2.js
@@ -2,3 +2,10 @@ module.exports = function (data) {
 
   return data;
 };
+const fruitsData = require ('../date.json');
+
+console.log (fruitsData);
+=======
+module.exports = function (data) {
+  return data;
+};
